0a3f6936110b6e0766df23c37de37c74a5d5dc3c
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-21 18:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let words = document.querySelectorAll("strong")
+
 function highlight() {
-    //Write your code here
-
-
+    words.forEach(word =>{
+		word.style.color = "rgb(0, 128, 0)";
+	})
 }
 
 
 function return_normal() {
-    //Write your code here
+     words.forEach(word =>{
+		word.style.color = "rgb(0, 0, 0)";
+	})
+}
 
     
-}
+
